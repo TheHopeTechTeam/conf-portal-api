a2d6eac7b983d1a0eead3efdc95fd65e6a55d3cf
Jay Hsia <jayhsia1997@gmail.com>
2026-04-30 13:52:40 -0400

    feat: Refine workshop filtering in ticket handler
    - Added conditions to exclude creative and leadership workshops from ticket retrieval queries, ensuring only relevant workshops are considered.
    - Introduced a new test for workshop registration status to validate the updated logic and enhance test coverage.

--- tests/handlers/test_ticket.py
+++ tests/handlers/test_ticket.py
@@ -2,6 +2,7 @@
 Test ticket handler
 """
 import os
+import uuid
 
 import pytest
 
@@ -18,3 +19,17 @@ async def test_get_user_ticket_by_email(ticket_handler: TicketHandler):
     email = os.environ.get("TEST_EMAIL")
     assert email
     result = await ticket_handler.get_user_ticket_by_email(email=email)
+
+
+@pytest.mark.asyncio
+async def test__get_workshop_registration_status(ticket_handler: TicketHandler):
+    """
+
+    :param ticket_handler:
+    :return:
+    """
+    user_id = os.environ.get("TEST_USER_ID")
+    assert user_id
+    user_id = uuid.UUID(user_id)
+    result = await ticket_handler._get_workshop_registration_status(user_id=user_id)
+    print(result)
